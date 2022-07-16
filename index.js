78f327a76307052f10c0e86e9617b33ca1ba4144
Balendra Singh <74133152+Balendrasingh2210@users.noreply.github.com>
2022-07-16 13:38:02 +0530

    Ready for production

--- index.js
+++ index.js
@@ -1,25 +1,23 @@
-let str = document.getElementById("name").value
 
-let output = document.getElementById("output")
-
-let getGender = async () =>{
-    try{
-        let response = await fetch(`https://api.genderize.io/?name=${str}`)
-        let data = await response.json()
-        if(data.gender === null){
-            output.innerHTML = "<h2>Please enter correct name</h2>"
-            output.style.color = "red"
-        }
-        else{
-            output.innerHTML = `<h2>${data.gender}</h2>`
-            output.style.color = "red"
-        }
-
-    }catch(error){
-        console.log(`${error}`)
-    }
-}
 
 function fn1(){
-    return getGender();
+    var str = document.getElementById("name").value
+    let fetches = fetch(`https://api.genderize.io/?name=${str}`)
+    // let data = response.json()
+    let output = document.getElementById("output")
+
+    fetches.then( res =>
+        res.json()).then(d=>{
+            // console.log(d.gender)
+            if(d.gender ===null ){
+                // console.log("Invalid name")
+                output.innerHTML = "<h2>Enter valid name</h2>"
+                output.style.color = "red"
+            }
+            else{
+                // console.log("name shi hai")
+                output.innerHTML = `<h2>${d.gender}</h2>`
+                output.style.color = "red"
+            }
+        })
 }
